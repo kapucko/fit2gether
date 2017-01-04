784714cbb86b8d1f74e066f37647ecc0c95d8f7a
Dominik Kapisinsky <dkapisinsky@cnc.sk>
2017-01-04 09:14:24 +0100

    get db conf from env

--- fit2gether/settings.py
+++ fit2gether/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,6 +123,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
